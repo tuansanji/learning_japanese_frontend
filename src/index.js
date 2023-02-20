b94aa5ee625822548cf1e13a6b43a9f2c6cbaeb3
SANJI <hoangtuan17012015@gmail.com>
2023-02-21 00:44:06 +0700

    change dotenv

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-import * as dotenv from "dotenv";
-dotenv.config();
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
