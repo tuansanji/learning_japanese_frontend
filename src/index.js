d47b7b1a008517c7bb2553febf964d88e2589d96
SANJI <hoangtuan17012015@gmail.com>
2023-02-28 07:41:15 +0700

    change homepage package

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashBrower as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
